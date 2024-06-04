aa17639a1e9ae38d143a6f3ef92d6b1c93cf3e4e
Simon Neidig <simon1234@online.de>
2024-06-04 17:03:59 +0200

    Initialize dotenv usage in fastapi

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
+# Import external dependencies
 from fastapi import FastAPI
 
+# Import internal dependencies
 from app.api.routes.education import education
 
+
+# Initialize FastAPI app
 app = FastAPI()
 
+# Add routes to FastAPI app
 app.include_router(education.router)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
