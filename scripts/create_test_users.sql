973fa4659c128930274451e60cffd9cf46faaaab
Simon Neidig <simon1234@online.de>
2025-09-07 17:42:47 +0200

    Update pipeline

--- scripts/create_test_users.sql
+++ scripts/create_test_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO public."user"
+INSERT INTO postgres."user"
 (id, email, hashed_password, is_active, is_superuser, is_verified)
 VALUES('3e98d38d-abd5-48a7-b3a8-449a46f0dc1c'::uuid, 'test@simon-neidig.eu', '$argon2id$v=19$m=65536,t=3,p=4$sxYXzoo116TYwN9pKlOfXw$x+J4qAbl3oQ5A2KMLQUwhX/jaJts4QgSWcBp8VLZ6N0', true, true, true);
-INSERT INTO public."user"
+INSERT INTO postgres."user"
 (id, email, hashed_password, is_active, is_superuser, is_verified)
 VALUES('9d9d2265-07ac-48d5-af14-a56e93233f04'::uuid, 'nonadmin@simon-neidig.eu', '$argon2id$v=19$m=65536,t=3,p=4$ZFtNuFL5zDG629ZUhqlkaA$9gWoPJswDHqH7gJhCmf6v6U31SNlIku46xFuvjYCe5A', true, false, false);
