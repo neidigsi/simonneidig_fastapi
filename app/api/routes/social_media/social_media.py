0a906a509bdf4fb2b03c50abee504d4db158ca99
Simon Neidig <simon1234@online.de>
2025-09-14 13:40:31 +0200

    Implement POST route for social media

--- app/api/routes/social_media/social_media.py
+++ app/api/routes/social_media/social_media.py
@@ -11,13 +11,19 @@ Main features:
 """
 
 # Import external dependencies
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 # Import internal dependencies
 from app.db.queries import social_media as crud
 from app.schemas import social_media as schemas
+from app.services.i18n import get_language
 from app.services.db import get_async_session
+from app.services.user import fastapi_users
+
+
+# dependency that enforces the current user to be a superuser
+get_current_superuser = fastapi_users.current_user(superuser=True)
 
 
 # Create a new APIRouter instance for the social media API
@@ -28,7 +34,7 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[schemas.SocialMedia])
+@router.get("/", response_model=list[schemas.SocialMediaRead])
 async def get_social_medias(db: AsyncSession = Depends(get_async_session)):
     """
     Retrieves a list of social media entries.
@@ -40,3 +46,33 @@ async def get_social_medias(db: AsyncSession = Depends(get_async_session)):
         list[SocialMedia]: List of social media links.
     """
     return await crud.get_social_medias(db)
+
+
+
+@router.post("/", response_model=schemas.SocialMediaRead, status_code=status.HTTP_201_CREATED)
+async def create_social_media(
+    payload: schemas.SocialMediaCreate,
+    lang: str = Depends(get_language),
+    db: AsyncSession = Depends(get_async_session),
+    _admin=Depends(get_current_superuser),
+    response: Response = None,
+):
+    """
+    Create a new education entry (admin only).
+    """
+    sm = await crud.create_social_media(
+        db,
+        name=payload.name,
+        url=payload.url,
+        color=payload.color,
+        path=payload.path
+    )
+    
+    if not sm:
+        raise HTTPException(status_code=500, detail="Failed to create social media")
+    
+        # set the language in the response headers so clients can easily detect the content language
+    if response is not None:
+        response.headers["Content-Language"] = lang
+    
+    return sm
